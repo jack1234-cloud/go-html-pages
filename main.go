f697f5f33e04933c7d90c39a93bff8bc7a7e1ae3
joflynn <jack.oflynn@aig.com>
2022-02-24 11:50:34 +0000

    updates

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+func login(w http.ResponseWriter, r *http.Request) {
+	var fileName = "login.html"
+	t, err := template.ParseFiles(fileName)
+	if err != nil {
+		fmt.Println("Error occurred parsing the file", err)
+		return
+	}
+	t.ExecuteTemplate(w, fileName, nil)
+	if err != nil {
+		fmt.Println("Error when executing template", err)
+		return
+	}
+}
+
+func loginSubmit(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
@@ -15,6 +34,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", login)
 	http.ListenAndServe("", nil)
 }
