af4656424026c4f4ba6734d5e3672f882ff8f610
joflynn <jack.oflynn@aig.com>
2022-02-24 12:19:55 +0000

    updates

--- main.go
+++ main.go
@@ -13,27 +13,43 @@ func login(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Error occurred parsing the file", err)
 		return
 	}
-	t.ExecuteTemplate(w, fileName, nil)
+	err = t.ExecuteTemplate(w, fileName, nil)
 	if err != nil {
 		fmt.Println("Error when executing template", err)
 		return
 	}
 }
 
-func loginSubmit(w http.ResponseWriter, r *http.Request) {
+var userDB = map[string]string{
+	"jack": "password",
+}
 
+func loginSubmit(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	if userDB[username] == password {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "User logged in successfully")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "User not found in DB")
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/login":
+		login(w, r)
 	case "/login-submit":
+		loginSubmit(w, r)
 	default:
-		fmt.Fprint(w, "Hi Jack")
+		fmt.Fprintf(w, "Hi Jack")
 	}
 }
 
 func main() {
-	http.HandleFunc("/", login)
+	http.HandleFunc("/", handler)
 	http.ListenAndServe("", nil)
+	// http.ListenAndServeTLS("", "cert.pem", "key.pem", nil)
 }
